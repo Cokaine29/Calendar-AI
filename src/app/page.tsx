8a3ab5da2322b7e00b612c3df20806d1efce716e
Niraj Kamble <niraj2835@gmail.com>
2026-08-26 19:02:10 +0530

    Import signOut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session, status } = useSession();
